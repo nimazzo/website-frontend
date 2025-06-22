50264b6b042ac27dfaa03587a506f1c7e195d27b
nimazzo <46820077+nimazzo@users.noreply.github.com>
2025-06-22 12:17:39 +0200

    Added more icons

--- src/homepage/data/icons.tsx
+++ src/homepage/data/icons.tsx
@@ -18,6 +18,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import DescriptionIcon from '@mui/icons-material/Description';
 import StorageIcon from '@mui/icons-material/Storage';
+import MonitorIcon from '@mui/icons-material/Monitor';
 import {FaReact} from 'react-icons/fa';
 
 export const icons: Record<string, string | ReactNode> = {
@@ -57,7 +58,8 @@ export const icons: Record<string, string | ReactNode> = {
   'Postman': <MailOutlineIcon color="primary" sx={{mr: 1, mt: 0.5}}/>,
   'Swagger/OpenAPI': <DescriptionIcon color="primary" sx={{mr: 1, mt: 0.5}}/>,
   'React': <FaReact color="#61DBFB" style={{marginRight: 8, marginTop: 8}}/>,
-  'Postgres/SQL': <StorageIcon color="primary" sx={{mr: 1, mt: 1}}/>
+  'Postgres/SQL': <StorageIcon color="primary" sx={{mr: 1, mt: 1}}/>,
+  'JavaFX': <MonitorIcon color="primary" sx={{mr: 1, mt: 1}}/>
 };
 
 
